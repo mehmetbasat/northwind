8185a4d5d6925de3ea52d7ab2a2cd1f785ffadf6
mehmet basat <80039024+mehmetbasat@users.noreply.github.com>
2024-06-28 22:44:18 +0300

    Product service added.

---
+++ src/main/java/kodlama/io/northwind/business/concretes/ProductManager.java
@@ -0,0 +1,25 @@
+package kodlama.io.northwind.business.concretes;
+
+import kodlama.io.northwind.business.abstracts.ProductService;
+import kodlama.io.northwind.dataAccess.abstracts.ProductDao;
+import kodlama.io.northwind.entities.concretes.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductManager implements ProductService {
+
+    private ProductDao productDao;
+
+    @Autowired
+    public ProductManager(ProductDao productDao) {
+        this.productDao = productDao;
+    }
+
+    @Override
+    public List<Product> getAll() {
+        return productDao.findAll();
+    }
+}
