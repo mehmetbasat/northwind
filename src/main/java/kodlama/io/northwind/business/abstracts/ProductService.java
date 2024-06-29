68adc5060d958214135eaeb98453938042650a14
mehmet basat <80039024+mehmetbasat@users.noreply.github.com>
2024-06-29 13:23:18 +0300

    Methods turned to Result

--- src/main/java/kodlama/io/northwind/business/abstracts/ProductService.java
+++ src/main/java/kodlama/io/northwind/business/abstracts/ProductService.java
@@ -1,9 +1,12 @@
 package kodlama.io.northwind.business.abstracts;
 
+import kodlama.io.northwind.core.utilities.results.DataResult;
+import kodlama.io.northwind.core.utilities.results.Result;
 import kodlama.io.northwind.entities.concretes.Product;
 
 import java.util.List;
 
 public interface ProductService {
-    List<Product> getAll();
+   DataResult<List<Product>> getAll();
+   Result add(Product product);
 }
